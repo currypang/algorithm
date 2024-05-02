89fbf82790a7c6739ca8a307696b0622cd562b4b
romesway <67998157+romesway@users.noreply.github.com>
2024-05-02 09:55:43 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,9 +1,9 @@
 function solution(arr) {
     var answer = 0;
-    let i = 0;
+    let i = 0
     while (i < arr.length) {
-           answer += arr[i];
-           i++;
-           }
+        answer = answer + arr[i];
+        i++;
+    }
     return answer / arr.length;
 }
