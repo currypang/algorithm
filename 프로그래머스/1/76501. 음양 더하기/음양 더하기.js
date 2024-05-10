5d18c20beff4f1198f5fea10c7defbc3078e8679
romesway <67998157+romesway@users.noreply.github.com>
2024-05-10 09:54:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -2,9 +2,9 @@ function solution(absolutes, signs) {
     let answer = 0;
     for (let i = 0; i < absolutes.length; i++) {
         if (signs[i]) {
-            answer = answer + absolutes[i]
+            answer = answer + absolutes[i];
         } else {
-            answer = answer - absolutes[i]
+            answer = answer - absolutes[i];
         }
     }
     return answer;
