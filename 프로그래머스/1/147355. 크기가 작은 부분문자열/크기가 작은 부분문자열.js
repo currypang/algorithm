1f1a97f8d932b079877b138621f9165e4570856d
currypang <67998157+currypang@users.noreply.github.com>
2024-05-26 15:29:59 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,9 @@
+function solution(t, p) {
+    let answer = 0;
+    for(let i = 0; i <= t.length - p.length; i++) {
+        if(Number(t.substr(i, p.length)) <= Number(p)) {
+            answer++
+        }
+    }
+    return answer;
+}
