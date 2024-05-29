7752d8f1a5072fa34b93d875ea6ac1e65426726b
currypang <67998157+currypang@users.noreply.github.com>
2024-05-30 00:40:51 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,23 @@
+function solution(s) {
+  let answer = s;
+  const word = {
+    zero: "0",
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+  };
+
+  for (const key in word) {
+    while (answer.includes(key)) {
+      answer = answer.replace(key, word[key]);
+    }
+  }
+
+  return Number(answer);
+}
