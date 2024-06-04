187ac816d6a2a0888c27ef1c6aabdbb358f1103b
currypang <67998157+currypang@users.noreply.github.com>
2024-06-05 00:39:00 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -1,13 +1,22 @@
 function solution(food) {
-  let left = [];
-  let right = [];
-  
-  for (let i = 1; i < food.length; i++) {
-    let count = Math.floor(food[i] / 2);
-    let str = String(i).repeat(count);
-    left.push(str);
-    right.unshift(str); 
-  }
-    
-  return left.join('') + "0" + right.join('');
+    let answer = '';
+    let left = [];
+    let right = [];
+    const arr = food.map((el) => {
+        if(el > 1 && el % 2) {
+            return el - 1;
+        } 
+        if (el > 1 && !(el % 2)) {
+            return el;
+        }
+         
+    })
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i]/2; j++) {
+            left.push(i);
+        }
+    }
+  right = [...left].reverse()
+    answer = [...left, 0, ...right].join("")
+    return answer;
 }
