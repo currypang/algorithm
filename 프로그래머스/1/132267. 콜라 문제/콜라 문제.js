04e2cf3b02c92cf97aeba062c678b4fde6e0895c
currypang <67998157+currypang@users.noreply.github.com>
2024-06-05 09:54:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,8 @@
 function solution(a, b, n) {
     let answer = 0;
-    let i = 0
     while(n >= a) {
         answer = answer + Math.floor(n / a) * b;
-        n = Math.floor(n / a) * b + n % a
-        
+        n = Math.floor(n / a) * b + n % a;
     }
     return answer;
 }
