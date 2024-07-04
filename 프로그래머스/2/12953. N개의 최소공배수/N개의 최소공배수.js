29428090f5db86af1e4cfd66018d5be1d74351a8
currypang <67998157+currypang@users.noreply.github.com>
2024-07-05 02:58:49 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  function gcd(a, b) {
+    if (b === 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  }
+
+  function lcm(a, b) {
+    return (a * b) / gcd(a, b);
+  }
+
+  const answer = arr.reduce((acc, cur) => lcm(acc, cur));
+
+  return answer;
+}
